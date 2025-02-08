3dd16fcbfaf2b35a67b2a34bd528de4f89d0aee2
Fgaoxing <92655031+Fgaoxing@users.noreply.github.com>
2025-02-08 19:12:38 +0800

    [style] gofmt

--- generator.go
+++ generator.go
@@ -31,7 +31,7 @@ func execCommand(command string) {
 
 type Generator struct {
 	DefindFunc  *Block
-	Root        *Block
+	Root        Blocks
 	Text        *os.File
 	Text2       *bytes.Buffer
 	ConstLength int
@@ -41,111 +41,61 @@ type Generator struct {
 	Dest        string
 }
 
+// Create a new Generator and generate the code
 func (g *Generator) New(fpath string) {
 	if g.PackageName == "" {
 		g.PackageName = "template"
 	}
 	g.Text2 = bytes.NewBuffer(nil)
-	g.Root, g.RawCode, g.DefindFunc = Parse(fpath)
+	g.Root, g.RawCode, g.DefindFunc = (&Parser{}).Parse(fpath)
 	file, err := os.Create(path.Join(g.Dest, path.Base(fpath)+".go"))
 	if err != nil {
 		panic(err.Error())
 	}
 	file.WriteString("// DO NOT EDIT!\n// Generate By Goh\n\n")
 	g.Text = file
-	g.Generate(nil)
+	g.generate()
 	execCommand("goimports -w .")
 	execCommand("gofmt -w .")
 	fmt.Println("\033[0;32mSuccess\033[0m", fpath)
 }
 
-func (g *Generator) Generate(b *Block) {
-	if b == nil {
-		g.Text.WriteString("package ")
-		g.Text.WriteString(g.PackageName)
-		g.Text.WriteString("\nimport (\n\t\"bytes\"\n\t\"github.com/OblivionOcean/Goh/utils\"\n)\n\n")
-		if g.DefindFunc == nil {
-			return
-		}
-		code, name, err := g.GenerateFunc(g.DefindFunc)
-		if err != nil {
-			panic(err.Error())
-		}
-		g.BufName = name
-		g.Text.WriteString(g.RawCode)
-		g.Text.WriteString(code)
-		g.Text.WriteString(fmt.Sprintf("{\n%s.Grow(", name))
-		b = g.Root
+func (g *Generator) generate() {
+	g.Text.WriteString("package ")
+	g.Text.WriteString(g.PackageName)
+	g.Text.WriteString("\nimport (\n\t\"bytes\"\n\t\"github.com/OblivionOcean/Goh/utils\"\n)\n\n")
+	if g.DefindFunc == nil {
+		return
 	}
-	switch b.Type {
-	case Code:
-		g.Text2.WriteString(b.Content)
-		g.Text2.WriteString("\n")
-	case HTML:
-		if len(strings.Trim(b.Content, " ")) == 0 {
-			break
-		}
-		tmp := "%s.WriteString(`%s`)\n"
-		g.ConstLength += len(b.Content)
-		g.Text2.WriteString(fmt.Sprintf(tmp, g.BufName, b.Content))
-	case Escape:
-		b.Content = strings.Trim(b.Content, " ")
-		if len(b.Content) == 0 {
-			break
-		}
-		var tmp string
-		switch b.VarType {
-		case String:
-			tmp = fmt.Sprintf("Goh.EscapeHTML(%s, %s)\n", b.Content, g.BufName)
-		case Bytes:
-			tmp = fmt.Sprintf("Goh.EscapeHTML(Goh.Bytes2String(%s), %s)\n", b.Content, g.BufName)
-		case Int:
-			tmp = fmt.Sprintf("Goh.FormatInt(int64(%s), %s)\n", b.Content, g.BufName)
-		case Uint:
-			tmp = fmt.Sprintf("Goh.FormatUint(uint64(%s), %s)\n", b.Content, g.BufName)
-		case Bool:
-			tmp = fmt.Sprintf("Goh.FormatBool(%s, %s)\n", b.Content, g.BufName)
-			g.ConstLength += 5
-		case Any:
-			tmp = fmt.Sprintf("Goh.FormatAny(%s, %s)\n", b.Content, g.BufName)
-		}
-		g.Text2.WriteString(tmp)
-	case Val:
-		b.Content = strings.Trim(b.Content, " ")
-		if len(b.Content) == 0 {
-			break
-		}
-		var tmp string
-		switch b.VarType {
-		case String:
-			tmp = fmt.Sprintf("%s.WriteString(%s)\n", g.BufName, b.Content)
-		case Bytes:
-			tmp = fmt.Sprintf("%s.Write(%s)\n", g.BufName, b.Content)
-				case Int:
-			tmp = fmt.Sprintf("Goh.FormatInt(int64(%s), %s)\n", b.Content, g.BufName)
-		case Uint:
-			tmp = fmt.Sprintf("Goh.FormatUint(uint64(%s), %s)\n", b.Content, g.BufName)
-		case Bool:
-			tmp = fmt.Sprintf("Goh.FormatBool(%s, %s)\n", b.Content, g.BufName)
-			g.ConstLength += 5
-		case Any:
-			tmp = fmt.Sprintf("Goh.FormatAny(%s, %s)\n", b.Content, g.BufName)
-		}
-		g.Text2.WriteString(tmp)
+	code, name, err := g.gFunc(g.DefindFunc)
+	if err != nil {
+		panic(err.Error())
 	}
-	for i := 0; i < len(b.Children); i++ {
-		g.Generate(b.Children[i])
+	g.BufName = name
+	g.Text.WriteString(g.RawCode)
+	g.Text.WriteString(code)
+	g.Text.WriteString(fmt.Sprintf("{\n%s.Grow(", name))
+	for i := 0; i < len(g.Root); i++ {
+		b := g.Root[i]
+		switch b.Type {
+		case Code:
+			g.Text2.WriteString(b.Content)
+			g.Text2.WriteString("\n")
+		case HTML:
+			g.gHTML(b)
+		case Escape:
+			g.gEscape(b)
+		case Val:
+			g.gVal(b)
+		}
 	}
-	if b == g.Root {
-		g.Text.WriteString(strconv.Itoa(g.ConstLength))
-		g.Text.WriteString(")\n")
-		g.Text.ReadFrom(g.Text2)
-		g.Text.WriteString("}\n")
-	}
-
+	g.Text.WriteString(strconv.Itoa(g.ConstLength))
+	g.Text.WriteString(")\n")
+	g.Text.ReadFrom(g.Text2)
+	g.Text.WriteString("}\n")
 }
 
-func (g *Generator) GenerateFunc(b *Block) (code string, name string, err error) {
+func (g *Generator) gFunc(b *Block) (code string, name string, err error) {
 	src := []byte("package Goh\n")
 	src = append(src, b.Content...)
 
@@ -157,12 +107,12 @@ func (g *Generator) GenerateFunc(b *Block) (code string, name string, err error)
 
 	funcDecl, ok := file.Decls[0].(*ast.FuncDecl)
 	if !ok {
-		return "", "", errors.New("Definition is not function type")
+		return "", "", errors.New("definition is not function type")
 	}
 
 	params := funcDecl.Type.Params.List
 	if len(params) == 0 {
-		err = errors.New("Definition parameters should not be empty")
+		err = errors.New("definition parameters should not be empty")
 		return
 	}
 
@@ -173,12 +123,12 @@ func (g *Generator) GenerateFunc(b *Block) (code string, name string, err error)
 	}
 	selectorExpr, ok := expr.(*ast.SelectorExpr)
 	if !ok {
-		err = errors.New("Definition parameters should not be empty")
+		err = errors.New("definition parameters should not be empty")
 		return
 	}
 
 	if selectorExpr.X.(*ast.Ident).Name != "bytes" && selectorExpr.Sel.Name != "Buffer" {
-		err = errors.New("Definition parameters should not be empty")
+		err = errors.New("definition parameters should not be empty")
 		return
 	}
 	if n := len(lastParam.Names); n > 0 {
@@ -186,4 +136,61 @@ func (g *Generator) GenerateFunc(b *Block) (code string, name string, err error)
 	}
 	code = b.Content
 	return
+}
+
+func (g *Generator) gVal(b *Block) {
+	b.Content = strings.Trim(b.Content, " ")
+	if len(b.Content) == 0 {
+		return
+	}
+	var tmp string
+	switch b.VarType {
+	case String:
+		tmp = fmt.Sprintf("%s.WriteString(%s)\n", g.BufName, b.Content)
+	case Bytes:
+		tmp = fmt.Sprintf("%s.Write(%s)\n", g.BufName, b.Content)
+	case Int:
+		tmp = fmt.Sprintf("Goh.FormatInt(int64(%s), %s)\n", b.Content, g.BufName)
+	case Uint:
+		tmp = fmt.Sprintf("Goh.FormatUint(uint64(%s), %s)\n", b.Content, g.BufName)
+	case Bool:
+		tmp = fmt.Sprintf("Goh.FormatBool(%s, %s)\n", b.Content, g.BufName)
+		g.ConstLength += 5
+	case Any:
+		tmp = fmt.Sprintf("Goh.FormatAny(%s, %s)\n", b.Content, g.BufName)
+	}
+	g.Text2.WriteString(tmp)
+}
+
+func (g *Generator) gEscape(b *Block) {
+	b.Content = strings.Trim(b.Content, " ")
+	if len(b.Content) == 0 {
+		return
+	}
+	var tmp string
+	switch b.VarType {
+	case String:
+		tmp = fmt.Sprintf("Goh.EscapeHTML(%s, %s)\n", b.Content, g.BufName)
+	case Bytes:
+		tmp = fmt.Sprintf("Goh.EscapeHTML(Goh.Bytes2String(%s), %s)\n", b.Content, g.BufName)
+	case Int:
+		tmp = fmt.Sprintf("Goh.FormatInt(int64(%s), %s)\n", b.Content, g.BufName)
+	case Uint:
+		tmp = fmt.Sprintf("Goh.FormatUint(uint64(%s), %s)\n", b.Content, g.BufName)
+	case Bool:
+		tmp = fmt.Sprintf("Goh.FormatBool(%s, %s)\n", b.Content, g.BufName)
+		g.ConstLength += 5
+	case Any:
+		tmp = fmt.Sprintf("Goh.FormatAny(%s, %s)\n", b.Content, g.BufName)
+	}
+	g.Text2.WriteString(tmp)
+}
+
+func (g *Generator) gHTML(b *Block) {
+	if len(strings.Trim(b.Content, " ")) == 0 {
+		return
+	}
+	tmp := "%s.WriteString(`%s`)\n"
+	g.ConstLength += len(b.Content)
+	g.Text2.WriteString(fmt.Sprintf(tmp, g.BufName, b.Content))
 }
--- parser.go
+++ parser.go
@@ -16,7 +16,7 @@ const (
 	Val
 	HTML
 	Code
-	RawCode
+	rawCode
 )
 
 const (
@@ -35,146 +35,161 @@ type Block struct {
 	Parent   *Block
 	Content  string
 	VarType  int
-	Unuse    bool
 }
 
-func (b *Block) AddChild(child *Block) {
-	if len(b.Children) > 0 && child.Type == HTML && b.Children[len(b.Children)-1].Type == HTML {
-		if len(b.Children[len(b.Children)-1].Content) > 0 {
-			b.Children[len(b.Children)-1].Content += child.Content
+type Parser struct {
+	text       string
+	cursor     int
+	endCursor  int
+	root       Blocks
+	defindFunc *Block
+	rawCode    string
+	fpath      string
+}
+
+type Blocks []*Block
+
+func (b *Blocks) addChild(child *Block) {
+	if len(*b) > 0 && child.Type == HTML && (*b)[len(*b)-1].Type == HTML {
+		if len((*b)[len(*b)-1].Content) > 0 {
+			(*b)[len(*b)-1].Content += child.Content
 		} else {
-			b.Children[len(b.Children)-1] = child
+			(*b)[len(*b)-1] = child
 		}
 	} else {
-		b.Children = append(b.Children, child)
-		child.Parent = b
+		*b = append(*b, child)
 	}
 }
 
-func Parse(fpath string) (*Block, string, *Block) {
-	// 将结构体变为局部变量，优化汇编代码跳转花费的时间
-	var (
-		Text       string
-		Curser     int
-		EndCurser  int
-		Root       *Block
-		DefindFunc *Block
-		RawCode    string
-	)
+// Parse Template File
+func (p *Parser) Parse(fpath string) (Blocks, string, *Block) {
+	p.text = readFile(fpath)
+	p.root = Blocks{}
+	p.fpath = fpath
+	for {
+		p.cursor = strings.Index(p.text, "<%")
+		if p.cursor == -1 {
+			p.root.addChild(&Block{
+				Content: p.text,
+				Type:    HTML,
+			})
+			break
+		}
+		p.root.addChild(&Block{
+			Content: p.text[:p.cursor],
+			Type:    HTML,
+		})
+		for {
+			p.text = p.text[p.cursor+2:]
+			p.endCursor = strings.Index(p.text, "%>")
+			if p.cursor == -1 {
+				panic("Syntax error")
+			}
+			if Goh.CountByte(p.text[:p.endCursor], '`')%2 != 0 || Goh.CountByte(p.text[:p.endCursor], '"')%2 != 0 {
+				p.cursor = p.endCursor
+				continue
+			} else {
+				switch p.text[0] {
+				case '=', '-':
+					p.pHTML()
+				case '#':
+				case '+':
+					p.pInclude()
+				case ':':
+					p.defindFunc = &Block{
+						Content: strings.Trim(p.text[1:p.endCursor], "\n\t\r "),
+					}
+				case '!':
+					p.rawCode += p.text[1:p.endCursor] + "\n"
+				default:
+					p.root.addChild(&Block{
+						Type:    Code,
+						Content: p.text[:p.endCursor],
+					})
+				}
+				p.text = p.text[p.endCursor+2:]
+				break
+			}
+		}
+		if 2 >= len(p.text) {
+			p.root.addChild(&Block{
+				Content: p.text,
+				Type:    HTML,
+			})
+			break
+		}
+	}
+	return p.root, p.rawCode, p.defindFunc
+}
+
+func readFile(fpath string) string {
 	file, err := os.OpenFile(fpath, os.O_RDONLY, 0644)
 	if err != nil {
 		panic(err.Error())
 	}
 	tmp, err := io.ReadAll(file)
-	Text = Goh.Byte2String(tmp)
 	if err != nil {
 		panic(err.Error())
 	}
-	Root = &Block{}
-	for {
-		Curser = strings.Index(Text, "<%")
-		if Curser == -1 {
-			Root.AddChild(&Block{
-				Content: Text,
-				Type:    HTML,
-			})
-			break
-		}
-		Root.AddChild(&Block{
-			Content: Text[:Curser],
-			Type:    HTML,
-		})
-		for {
-			Text = Text[Curser+2:]
-			EndCurser = strings.Index(Text, "%>")
-			if Curser == -1 {
-				panic("Syntax error")
-			}
-			if Goh.CountByte(Text[:EndCurser], '`')%2 != 0 || Goh.CountByte(Text[:EndCurser], '"')%2 != 0 {
-				Curser = EndCurser
-				continue
-			} else {
-				switch Text[0] {
-				case '=', '-':
-					block := &Block{
-						Type: Escape,
-					}
-					if Text[0] == '-' {
-						block.Type = Val
-						Text = Text[1:]
-						EndCurser -= 1
-					} else if Text[1] == '=' {
-						block.Type = Val
-						Text = Text[2:]
-						EndCurser -= 2
-					} else {
-						Text = Text[1:]
-						EndCurser -= 1
-					}
-					if Text[1] == ' ' || Text[2] == ' ' {
-						switch Text[0] { // 使用switch，底层是cmp比较，不是树遍历，速度更快
-						case 'b':
-							if Text[1] == 's' {
-								block.VarType = Bytes
-							} else {
-								block.VarType = Bool
-							}
-						case 'u':
-							block.VarType = Uint
-						case 'i':
-							block.VarType = Int
-						case 's':
-							block.VarType = String
-						case 'f':
-							block.VarType = Float
-						case 'v', 'a':
-							block.VarType = Any
-						default:
-							block.VarType = String
-						}
-						if Text[1] == 's' {
-							Text = Text[2:]
-							EndCurser -= 2
-						} else {
-							Text = Text[1:]
-							EndCurser -= 1
-						}
-					} else {
-						block.VarType = String
-					}
-					block.Content = Text[:EndCurser]
-					Root.AddChild(block)
-				case '#':
-				case '+':
-					fpath := path.Join(path.Dir(fpath), strings.Trim(Text[1:EndCurser], " \"\t\n\r"))
-					tmp, rawCode, _ := Parse(fpath)
-					RawCode += rawCode
-					for i := 0; i < len(tmp.Children); i++ {
-						Root.AddChild(tmp.Children[i])
-					}
-				case ':':
-					DefindFunc = &Block{
-						Content: strings.Trim(Text[1:EndCurser], "\n\t\r "),
-					}
-				case '!':
-					RawCode += Text[1:EndCurser] + "\n"
-				default:
-					Root.AddChild(&Block{
-						Type:    Code,
-						Content: Text[:EndCurser],
-					})
-				}
-				Text = Text[EndCurser+2:]
-				break
-			}
-		}
-		if 2 >= len(Text) {
-			Root.AddChild(&Block{
-				Content: Text,
-				Type:    HTML,
-			})
-			break
-		}
+	return Goh.Byte2String(tmp)
+}
+
+func (p *Parser) pHTML() {
+	block := &Block{
+		Type: Escape,
 	}
-	return Root, RawCode, DefindFunc
+	if p.text[0] == '-' {
+		block.Type = Val
+		p.text = p.text[1:]
+		p.endCursor -= 1
+	} else if p.text[1] == '=' {
+		block.Type = Val
+		p.text = p.text[2:]
+		p.endCursor -= 2
+	} else {
+		p.text = p.text[1:]
+		p.endCursor -= 1
+	}
+	if p.text[1] == ' ' || p.text[2] == ' ' {
+		switch p.text[0] { // 使用switch，底层是cmp比较，不是树遍历，速度更快
+		case 'b':
+			if p.text[1] == 's' {
+				block.VarType = Bytes
+			} else {
+				block.VarType = Bool
+			}
+		case 'u':
+			block.VarType = Uint
+		case 'i':
+			block.VarType = Int
+		case 's':
+			block.VarType = String
+		case 'f':
+			block.VarType = Float
+		case 'v', 'a':
+			block.VarType = Any
+		default:
+			block.VarType = String
+		}
+		if p.text[1] == 's' {
+			p.text = p.text[2:]
+			p.endCursor -= 2
+		} else {
+			p.text = p.text[1:]
+			p.endCursor -= 1
+		}
+	} else {
+		block.VarType = String
+	}
+	block.Content = p.text[:p.endCursor]
+	p.root.addChild(block)
+}
+
+func (p *Parser) pInclude() {
+	fpath := path.Join(path.Dir(p.fpath), strings.Trim(p.text[1:p.endCursor], " \"\t\n\r"))
+	tmp, rawCode, _ := p.Parse(fpath)
+	rawCode += rawCode
+	for i := 0; i < len(tmp); i++ {
+		p.root.addChild(tmp[i])
+	}
 }
